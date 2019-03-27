9bda30e1c67cb571f4691edd55fd01ede6be1ab8
allexg <allexandra.gadioi@gmail.com>
2019-03-27 23:56:21 +0200

    Create 7.py

---
+++ python/7.py
@@ -0,0 +1,25 @@
+# https://projecteuler.net/problem=7
+# 10001st prime
+
+from math import sqrt
+
+
+def is_prime(n):
+    if n < 2:
+        return False
+    for d in range(2, int(sqrt(n)) + 1):
+        if n % d == 0:
+            return False
+    return True
+
+
+def nthPrime(n):
+    current = 2
+    while n > 0:
+        if is_prime(current):
+            n -= 1
+        current += 1
+    return current
+
+
+print nthPrime(10001)
