8e5fa45efc4992237323b46b03c31a5fb0194daa
allexg <allexandra.gadioi@gmail.com>
2019-03-30 12:01:49 +0200

    Update 9.py

--- python/9.py
+++ python/9.py
@@ -6,7 +6,7 @@
 # using the formula for generating pythagorean triplets: a = m^2 - n^2, b = 2mn, c = m^2 + n^2
 def find_pythagorean_triplets(sum):
     for n in range(sum + 1):
-        for m in range(n, sum + 1):
+        for m in range(n, sum + 1): # if inverse loops order (m first), we can also obtain the desired sum, but product will be negative
             a = m ** 2 - n ** 2
             b = 2 * m * n
             c = m ** 2 + n ** 2
@@ -15,7 +15,7 @@ def find_pythagorean_triplets(sum):
                 return a, b, c, a * b * c
             elif current_sum > sum:
                 break
-    raise Exception("Provided argument is not a sum of a pythagorean triplet")
+    raise Exception("Provided argument is not the sum of any pythagorean triplet")
 
 
 print find_pythagorean_triplets(1000)
