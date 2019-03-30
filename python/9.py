1555ddfb8e3d4ff8b0b105c32437193f12e70e6f
allexg <allexandra.gadioi@gmail.com>
2019-03-30 11:59:55 +0200

    Update 9.py

--- python/9.py
+++ python/9.py
@@ -1,2 +1,21 @@
 # https://projecteuler.net/problem=9
 # Special Pythagorean triplet
+
+
+# parameter represents the sum of the triplets
+# using the formula for generating pythagorean triplets: a = m^2 - n^2, b = 2mn, c = m^2 + n^2
+def find_pythagorean_triplets(sum):
+    for n in range(sum + 1):
+        for m in range(n, sum + 1):
+            a = m ** 2 - n ** 2
+            b = 2 * m * n
+            c = m ** 2 + n ** 2
+            current_sum = a + b + c # 2m(m+n)
+            if current_sum == sum:
+                return a, b, c, a * b * c
+            elif current_sum > sum:
+                break
+    raise Exception("Provided argument is not a sum of a pythagorean triplet")
+
+
+print find_pythagorean_triplets(1000)
